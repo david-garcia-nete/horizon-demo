4ba301f71034fa0a481d8ac4430735d7cefd325b
David Alan Garcia <david.garcia.nete@gmail.com>
2026-01-27 22:20:01 -0500

    Use official Horizon migrations

--- database/migrations/2018_10_30_000000_create_horizon_tables.php
+++ database/migrations/2018_10_30_000000_create_horizon_tables.php
@@ -12,26 +12,40 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('queue');
             $table->longText('payload');
-            $table->unsignedTinyInteger('status');
+            $table->string('status');
             $table->unsignedInteger('runtime')->nullable();
-            $table->text('exception')->nullable();
-            $table->timestamp('failed_at')->nullable();
+            $table->longText('exception')->nullable();
+            $table->timestamp('completed_at')->nullable();
             $table->timestamps();
         });
 
         Schema::create('horizon_failed_jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('uuid')->unique();
             $table->string('connection');
             $table->string('queue');
             $table->longText('payload');
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
         });
+
+        Schema::create('horizon_monitoring', function (Blueprint $table) {
+            $table->string('tag')->primary();
+            $table->timestamp('last_seen')->nullable();
+        });
+
+        Schema::create('horizon_snapshots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->string('key');
+            $table->longText('value');
+            $table->timestamp('created_at')->nullable();
+        });
     }
 
     public function down(): void
     {
+        Schema::dropIfExists('horizon_snapshots');
+        Schema::dropIfExists('horizon_monitoring');
         Schema::dropIfExists('horizon_failed_jobs');
         Schema::dropIfExists('horizon_jobs');
     }
